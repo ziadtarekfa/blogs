357068d088dec5d8aef11cbb102804a98847201b
Ziad Tarek <ziadtarekfa@gmail.com>
2023-10-08 11:29:33 +0300

    implemented posts api :backend

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { randomBytes } from "crypto";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (_req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString("hex");
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    }
+    res.status(201).send(posts[id]);
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
