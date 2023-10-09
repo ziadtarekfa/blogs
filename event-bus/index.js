c5ac6d79356be3d45df3f5ed0d1bb21126c654eb
Ziad Tarek <ziadtarekfa@gmail.com>
2023-10-09 12:47:50 +0300

    implemented Query service : backend

--- event-bus/index.js
+++ event-bus/index.js
@@ -18,7 +18,7 @@ app.post('/events', (req, res) => {
     const event = req.body;
     axios.post('http://localhost:3001/events', event);
     axios.post('http://localhost:4000/events', event);
-
+    axios.post('http://localhost:4004/events', event);
     res.send({ status: 'OK' });
 });
 
---
+++ query/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin: '*'
+}));
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = {
+            id, title, comments: []
+        };
+    }
+    else if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+    console.log(posts);
+    res.send({});
+});
+
+app.listen(4004, () => {
+    console.log("Listening on port 4004");
+});
